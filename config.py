47267c8fae59868dfa6ee56c341908ff6bc503e3
AbenOG <harry.wood.96.gr@gmail.com>
2022-03-07 00:04:22 +0200

    Update:
    Now the program checks for site status using playwright instead of requests because it's more accurate in detecting site status.
    
    Updated a few warning messages for better user interaction.
    
    Added new config param 'iscaptcha' so the program or the user manually can set it to 'true' or 'false'
    
    When 'iscaptcha' is enabled the script runs with headless mode turned OFF until it cools down.
    *fuck off google captcha*
    ....

--- config.py
+++ config.py
@@ -3,10 +3,17 @@ import sys
 import time
 from configparser import ConfigParser
 from colorama import Fore
-from main import getCfg
+from main import getCfg, dataPath, clear
+from playwright.sync_api import sync_playwright
 
 
 def readConfig():
+    with sync_playwright() as p:
+        browser = p.chromium.launch_persistent_context(f'{dataPath()}' + '\\browserData')
+        page = browser.new_page()
+        status = page.goto('https://www.skroutz.gr').status
+        status_ok = page.goto('https://www.skroutz.gr').ok
+
     cfg = ConfigParser()
 
     if not os.path.isfile('config.ini'):
@@ -16,6 +23,7 @@ def readConfig():
         cfg.set('config', 'bypass_csp', 'True')
         cfg.set('config', 'locale', 'en-US')
         cfg.set('config', 'timezone_id', 'Europe/Athens')
+        cfg.set('config', 'iscaptcha', 'False')
         cfg.set('config', 'headless', 'True')
 
         try:
@@ -39,24 +47,79 @@ def readConfig():
                                    'Proceeding..️\n')
         time.sleep(2)
         readConfig()
+
     else:
         cfg.read('config.ini', encoding='UTF-8')
 
-        js = cfg.getboolean('config', 'javascript')
-        bypass_csp = cfg.getboolean('config', 'bypass_csp')
-        locale = cfg.get('config', 'locale')
-        tmz_id = cfg.get('config', 'timezone_id')
-        headless = cfg.getboolean('config', 'headless')
+        if status_ok:
 
-        cfgValues = {
-            'js': js,
-            'csp': bypass_csp,
-            'locale': locale,
-            'tmz_id': tmz_id,
-            'headless': headless
-        }
+            cfg.set('config', 'iscaptcha', 'False')
 
-        return getCfg(cfgValues)
+            with open('config.ini', 'w', encoding='UTF-8') as conf:
+                cfg.write(conf)
+
+            js = cfg.getboolean('config', 'javascript')
+            bypass_csp = cfg.getboolean('config', 'bypass_csp')
+            locale = cfg.get('config', 'locale')
+            tmz_id = cfg.get('config', 'timezone_id')
+            isCaptcha = cfg.get('config', 'iscaptcha')
+            headless = cfg.getboolean('config', 'headless')
+
+            cfgValues = {
+                'js': js,
+                'csp': bypass_csp,
+                'locale': locale,
+                'tmz_id': tmz_id,
+                'isCaptcha': isCaptcha,
+                'headless': headless
+            }
+
+            return getCfg(cfgValues)
+
+        elif status == 429:
+
+            cfg.set('config', 'headless', 'False')
+            cfg.set('config', 'iscaptcha', 'True')
+
+            with open('config.ini', 'w', encoding='UTF-8') as conf:
+                cfg.write(conf)
+
+            js = cfg.getboolean('config', 'javascript')
+            bypass_csp = cfg.getboolean('config', 'bypass_csp')
+            locale = cfg.get('config', 'locale')
+            tmz_id = cfg.get('config', 'timezone_id')
+            isCaptcha = cfg.getboolean('config', 'iscaptcha')
+            headless = cfg.getboolean('config', 'headless')
+
+            cfgValues = {
+                'js': js,
+                'csp': bypass_csp,
+                'locale': locale,
+                'tmz_id': tmz_id,
+                'iscaptcha': isCaptcha,
+                'headless': headless
+            }
+            print(Fore.LIGHTRED_EX + '\n!! WARNING !! >[HEADLESS MODE TEMPORARILY DISABLED]<\n'
+                                     '========================================================\n'
+                                     'Because of the unusual number of requests coming from your network\n'
+                                     'Skroutz.gr has blocked your access unless you verify you\'re not a robot\n'
+                                     'Headless mode will not work during this phase, usually for about an hour or two.\n'
+                                     'For now this is the only way to get through this.\n'
+                                     'After you verify and solve the captcha, please wait a few seconds it\'s going to proceed automatically.\n'
+                                     'Note that you only have 20 seconds to solve the captcha\n'
+                                     'While in this phase, the program will continue starting in windowed mode instead of headless until it goes back to normal.\n')
+            print(Fore.LIGHTYELLOW_EX +
+                  'Note:\n'
+                  'Even after verification, the program will continue to execute in windowed mode.\n'
+                  'Headless mode is different than just opening a regular browser.\n'
+                  'The only thing you can do during this phase is to wait for it to cooldown.\n')
+            input(Fore.LIGHTWHITE_EX + 'Press enter to continue..')
+
+            clear()
+            return getCfg(cfgValues)
+        else:
+            print(
+                Fore.LIGHTRED_EX + 'Site or internet connection Offline, please check your network configuration and try again.')
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import re
 import sys
+import time
+
 from bs4 import BeautifulSoup as soup
 from colorama import Fore
 from playwright.sync_api import sync_playwright
@@ -25,7 +27,7 @@ def clear():
 
 def getTime():
     now = datetime.now()
-    dt = now.strftime("%d/%m/%Y %H-%M-%S")
+    dt = now.strftime("%d/%m/%Y %H:%M:%S")
     return dt
 
 
@@ -61,59 +63,104 @@ def readFile(cfg):
 
 
 def getContent(url, cfg):
+    doneCaptcha = None
+
     with sync_playwright() as p:
         if len(url) > 0:
             print(Fore.LIGHTGREEN_EX + 'Loading data..\n')
             for x in range(0, len(url)):
-                if x == 0:
+                if x == 0 and cfg.get('iscaptcha') and not doneCaptcha:
                     browser = p.chromium.launch_persistent_context(f'{dataPath()}' + '\\browserData',
                                                                    java_script_enabled=cfg.get('js'),
                                                                    bypass_csp=cfg.get('csp'),
                                                                    user_agent=user_agent,
                                                                    locale=cfg.get('locale'),
                                                                    headless=cfg.get('headless'),
-                                                                   timezone_id=cfg.get('tmz_id'))
-                page = browser.new_page()
+                                                                   timezone_id=cfg.get('tmz_id'),
+                                                                   no_viewport=True,
+                                                                   viewport=None,
+                                                                   args=["--window-size=800,800"],
+                                                                   timeout=20000)
+                    _page = browser.new_page()
+                    _page.goto('https://skroutz.gr')
+                    time.sleep(20)
+                    doneCaptcha = True
 
-                if url[x].endswith('#shops'):
-                    page.goto(url[x])
-                else:
-                    page.goto(url[x] + "#shops")
-
-                content = page.content()
-
-                s = soup(content, 'html.parser')
-
-                dominant_price = s.find_all(class_="dominant-price")
-                shop_name = s.find_all(class_="shop-name")
-                product_title = s.find_all("title")
-
-                shopName = []
-                shopPrice = []
-                productTitle = clean_html.sub('', str(product_title))
-
-                for names in shop_name:
-                    newName = str(names)
-                    cleanName = clean_html.sub('', newName)
-                    shopName.append(cleanName)
-
-                for price in dominant_price:
-                    newPrice = str(price)
-                    cleanPrice = clean_html.sub('', newPrice)
-                    try:
-                        shopPrice.append(
-                            float(cleanPrice.replace('.', '').replace(",", "").replace("€", "")) / 100)
-
-                        _min = min(shopPrice, default=0)
-                    except ValueError:
-                        break
-
-                if x == len(url) - 1:
-                    page.close()
+                if x == 0 and cfg.get('iscaptcha') and doneCaptcha:
                     browser.close()
-                    p.stop()
+                    browser = p.chromium.launch_persistent_context(f'{dataPath()}' + '\\browserData',
+                                                                   java_script_enabled=cfg.get('js'),
+                                                                   bypass_csp=cfg.get('csp'),
+                                                                   user_agent=user_agent,
+                                                                   locale=cfg.get('locale'),
+                                                                   headless=cfg.get('headless'),
+                                                                   timezone_id=cfg.get('tmz_id'),
+                                                                   no_viewport=True,
+                                                                   viewport=None,
+                                                                   args=["--window-size=600,480"])
+                if x == 0 and not cfg.get('iscaptcha'):
+                    browser = p.chromium.launch_persistent_context(f'{dataPath()}' + '\\browserData',
+                                                                   java_script_enabled=cfg.get('js'),
+                                                                   bypass_csp=cfg.get('csp'),
+                                                                   user_agent=user_agent,
+                                                                   locale=cfg.get('locale'),
+                                                                   headless=cfg.get('iscaptcha'),
+                                                                   timezone_id=cfg.get('tmz_id'))
+                if browser:
+                    page = browser.new_page()
 
-                processContent(shopName, shopPrice, productTitle, _min)
+                    if url[x].endswith('#shops'):
+                        page.goto(url[x])
+                    else:
+                        page.goto(url[x] + "#shops")
+
+                    time.sleep(.35)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    page.mouse.wheel(0, 2000)
+                    time.sleep(.25)
+                    content = page.content()
+
+                    s = soup(content, 'html.parser')
+
+                    dominant_price = s.find_all(class_="dominant-price")
+                    shop_name = s.find_all(class_="shop-name")
+                    product_title = s.find_all("title")
+
+                    shopName = []
+                    shopPrice = []
+                    productTitle = clean_html.sub('', str(product_title))
+
+                    for names in shop_name:
+                        newName = str(names)
+                        cleanName = clean_html.sub('', newName)
+                        shopName.append(cleanName)
+
+                    for price in dominant_price:
+                        newPrice = str(price)
+                        cleanPrice = clean_html.sub('', newPrice)
+                        try:
+                            shopPrice.append(
+                                float(cleanPrice.replace('.', '').replace(",", "").replace("€", "")) / 100)
+
+                            _min = min(shopPrice, default=0)
+                        except ValueError:
+                            break
+
+                    if x == len(url) - 1:
+                        page.close()
+                        browser.close()
+                        p.stop()
+
+                    processContent(shopName, shopPrice, productTitle, _min)
         else:
             clear()
             print(Fore.LIGHTRED_EX + 'No links inside the list, populate the list and try again.')
@@ -127,22 +174,22 @@ def processContent(shopName, price, title, _min):
 
     print(Fore.LIGHTMAGENTA_EX + f'\n\n{title:^15}')
 
-    print(Fore.LIGHTBLACK_EX + '==========================================')
-    print(Fore.LIGHTYELLOW_EX + f'{"Store":<15}{"Price":>15}\n')
+    print(Fore.LIGHTBLACK_EX + '===============================================================')
+    print(Fore.LIGHTYELLOW_EX + f'{"Store":<22}{"Price":>35}\n')
 
     try:
         for x in range(len(price)):
             if price[x] == _min and _min != price[x - 1]:
-                print(Fore.LIGHTGREEN_EX + f'{shopName[x]:<15}{price[x]:>15}  <- Lowest Price')
+                print(Fore.LIGHTGREEN_EX + f'{shopName[x]:<22}{price[x]:>35}  <- Lowest Price')
                 avg += price[x]
                 prodCount += 1
             else:
-                print(Fore.LIGHTWHITE_EX + f'{shopName[x]:<15}{price[x]:>15}')
+                print(Fore.LIGHTWHITE_EX + f'{shopName[x]:<22}{price[x]:>35}')
                 avg += price[x]
                 prodCount += 1
     except IndexError:
         pass
-    print(Fore.LIGHTBLACK_EX + '==========================================\n\n')
+    print(Fore.LIGHTBLACK_EX + '===============================================================\n\n')
 
     try:
         if not os.path.isfile(path):
@@ -152,6 +199,7 @@ def processContent(shopName, price, title, _min):
                             f'date: {getTime()}\n\n'
                             f'Product: {title}\n'
                             f'Average Price: {avg / prodCount:.2f}\n'
+                            f'From {prodCount} different stores\n'
                             f'Lowest price: {_min}\n\n')
         else:
             with (open(path, 'a', encoding='UTF-8')) as data:
@@ -159,6 +207,11 @@ def processContent(shopName, price, title, _min):
                            f'date: {getTime()}\n\n'
                            f'Product: {title}\n'
                            f'Average Price: {avg / prodCount:.2f}\n'
+                           f'From {prodCount} different stores\n'
                            f'Lowest price: {_min}\n\n')
     except ZeroDivisionError:
-        return print(Fore.LIGHTRED_EX + 'Something went wrong, Check your entries and try again.'), sys.exit()
+        return print(Fore.LIGHTRED_EX + 'Zero division error, meaning no products were added.\n'
+                                        '1. The website is offline or your internet connection dropped.\n'
+                                        '2. The website thinks you are a robot due to many requests and requires further manual verification\n'
+                                        '3. The links you provided are invalid.\n'
+                                        'If number 2 is true, rerun the program and solve the captcha.'), sys.exit()
